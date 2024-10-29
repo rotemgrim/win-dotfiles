140c99d1bb47fee113aed7690250ca50ff0b70d9
rotem <rotemgrim@gmail.com>
2024-10-30 01:48:28 +0200

    change nvim a little

--- AppData/Local/nvim/lua/config/keymaps.lua
+++ AppData/Local/nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- 35/15/2027
+
 -- keymaps
 local map = vim.keymap.set
 
@@ -11,4 +13,13 @@ map("n", "<c-_>", "<cmd>normal gcc<cr>j", { desc = "Comment line" })
 map("v", "<c-/>", "<cmd>normal gcc<cr>", { desc = "Comment Block" })
 map("v", "<c-_>", "<cmd>normal gcc<cr>", { desc = "Comment Block" })
 
-vim.api.nvim_set_keymap("n", "<C-q>", "<C-v>", { noremap = true })
+-- remap visual block mode to <C-q> (in windows <C-v> is used to paste)
+map("n", "<C-q>", "<C-v>", { noremap = true })
+
+-- esc to normal mode when pressing stuff
+map("i", "jk", "<esc>", { noremap = true })
+map("i", "kj", "<esc>", { noremap = true })
+map("i", "jf", "<esc>", { noremap = true })
+map("i", "fj", "<esc>", { noremap = true })
+map("i", "jj", "<esc>", { noremap = true })
+map("i", "ff", "<esc>", { noremap = true })
