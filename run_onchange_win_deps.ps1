6aec58c2cf5c779861928aa1cd4d80353d1e9b13
rotem <rotemgrim@gmail.com>
2024-11-17 17:35:39 +0200

    Update win_deps.ps1

--- run_onchange_win_deps.ps1
+++ run_onchange_win_deps.ps1
@@ -28,6 +28,8 @@ winget install Neovim.Neovim
 # Install Lazygit using Winget
 winget install jesseduffield.lazygit
 
+winget install eza-community.eza
+
 # Install LazyVim
 git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
 Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
